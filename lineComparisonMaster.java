e20e100d666f27e83f205ccea7c842741858e7f3
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-04-25 17:37:02 +0530

    [Swapnil] added lineComparison.java

---
+++ lineComparisonMaster.java
@@ -0,0 +1,6 @@
+import java.io.*;
+public class lineComparisonMaster{
+	public static void main(String args[]){
+		System.out.println("Wellcome To Line Comparison Computation Program On Master Branch");
+	}
+}
